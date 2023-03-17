89f4900d34f7d09f34e18c755ac4d85005d46207
AthiyyahRachmazaine9999 <atya89999@gmail.com>
2023-03-17 17:22:23 +0700

    salescost include detail tambahan js

--- public/ctrl/finance/totalsmenusdetails.js
+++ public/ctrl/finance/totalsmenusdetails.js
@@ -5,71 +5,7 @@ var DatatableCash = {
                 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
             }
         });
-        var table = $(".m_datatable").DataTable({
-            autoWidth: false,
-            columnDefs: [
-                { width: "30%", targets: [0] },
-                { width: "15%", targets: [2, 3, 4] },
-                {
-                    "visible": false,
-                    "searchable": false
-                },
-            ],
-            order: [
-                [3, "desc"],
-            ],
-            dom: '<"datatable-header"fl><"datatable-scroll"t><"datatable-footer"ip>',
-            ordering: true,
-            processing: true,
-            dataSrc: "",
-            language: {
-                search: '<span>Filter:</span> _INPUT_',
-                lengthMenu: '<span>Show:</span> _MENU_ Per Halaman',
-                paginate: {
-                    'first': 'First',
-                    'last': 'Last',
-                    'next': '&rarr;',
-                    'previous': '&larr;'
-                }
-            },
-            ajax: {
-                headers: {
-                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                },
-                url: main_url + "finance/settlement/{id}/setmenutotaldetails",
-
-                // totalsmenustotalsmenus
-
-
-                // totalsmenustotalsmenus
-                
-                type: 'get',
-                dataType: 'json',
-
-                data: {
-                    '_token': token
-                }
-            },
-
-
-            
-
-
-            columns: [{
-                    "data": "emp",
-                    
-                },
-                {
-                    "data": "costsetmenu",
-                    render: $.fn.dataTable.render.number(',', '.', 0)
-                },
-                
-            ]
-
-            
-            
-
-        });
+        var table = $(".salescostdetail").DataTable({});
     }
 };
 jQuery(document).ready(function() {
